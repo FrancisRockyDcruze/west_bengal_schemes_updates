b0949eca91c6d407743b2c8030b0a5f1ab68de51
FrancisRockyDcruze <87224437+FrancisRockyDcruze@users.noreply.github.com>
2026-02-21 23:52:52 +0530

    updated all the html pages and also added schemes folder

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-fetch("yuva_sathi.html")
+let basePath = window.location.pathname;
+console.log(basePath)
+
+if(basePath == "/" || basePath.includes("/index"))
+{
+fetch("latest_scheme.html")
     .then(response => response.text())
     .then(data => {
     document.getElementById("main_container").innerHTML = data;
@@ -10,4 +14,64 @@ fetch("yuva_sathi.html")
     document.getElementById("main_container").innerHTML =
         "<p class='text-red-500'>Failed to load content.</p>";
     });
+}
+
+
+// header
+// basePath = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/") + 1);
+let headerPath = basePath === "/" ? "header.html" : "../header.html";
+console.log("header : " + headerPath)
+
+fetch(headerPath)
+.then(response => response.text())
+.then(data => {
+document.getElementById("header_container").innerHTML = data;
+})
+.catch(error => {
+console.error("Error loading Yuva content:", error);
+
+document.getElementById("main_container").innerHTML =
+    "<p class='text-red-500'>Failed to load content.</p>";
+});
+
+
+
+// footer
+// document.addEventListener("DOMContentLoaded", function () {
+// basePath = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/") + 1);
+let footerPath = basePath === "/" ? "footer.html" : "../footer.html";
+console.log("foot : " + footerPath)
+
+fetch(footerPath)
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("footer_container").innerHTML = data;
+    })
+    .catch(error => {
+    console.error("Error loading Yuva content:", error);
+    
+    document.getElementById("main_container").innerHTML =
+        "<p class='text-red-500'>Failed to load content.</p>";
+    });
+
+
+document.addEventListener('click', function (e) {
+  if (e.target.closest('#mobile-menu-btn')) {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu) {
+      mobileMenu.classList.toggle('hidden');
+    }
+  }
+});
+
+
+document.addEventListener('click', function (e) {
+  if (e.target.closest('#mobile-schemes-btn')) {
+    const mobileSchemeMenu = document.getElementById('mobile-dropdown');
+    const mobiledropicon = document.getElementById('mobile-dropdown-icon');
+    if (mobileSchemeMenu) {
+      mobileSchemeMenu.classList.toggle('hidden');
+      mobiledropicon.classList.toggle('rotate-180');
+    }
+  }
 });
